6f2ae532066e4065377d4edc30075452942bc060
Mark Lennox <mlennox@Marks-MacBook-Air.local>
2013-04-14 17:16:07 +0100

    sketching out milestones to a working app

---
+++ assets/www/js/geotest.js
@@ -0,0 +1,16 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: mlennox
+ * Date: 14/04/2013
+ * Time: 17:08
+ * To change this template use File | Settings | File Templates.
+ */
+
+// steps toward a fully working app:
+// 1. load all js and display 'ready' message
+// 2. loop to check location and update view to display the data
+// 3. connect to google maps / open street maps
+// 3.a figure out what the data looks like and how we might go about displaying it
+// 4. load the region centered around the geo location and display it
+// next steps??
+
