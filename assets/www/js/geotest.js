727b84f248393ff62fab19e6056816fee53bf967
Mark Lennox <mlennox@Marks-MacBook-Air.local>
2013-04-20 00:03:34 +0100

    naive wiring of events and displaying details

--- assets/www/js/geotest.js
+++ assets/www/js/geotest.js
@@ -12,6 +12,7 @@
 // 3. connect to google maps / open street maps
 // 3.a figure out what the data looks like and how we might go about displaying it
 // 4. load the region centered around the geo location and display it
+// 5. show a fuzzy compass - "you are pointing at {nearest landmark}" or a nautical compass "nor by norwest" etc.
 // next steps??
 
 'use strict';
@@ -19,6 +20,11 @@
 var GeoTest = {};
 
 (function(app){
+
+    var watchId = null;
+
+    var elLatitude = null;
+    var elLongitude = null;
 
     var onDeviceReady = function() {
 
@@ -34,12 +40,40 @@ var GeoTest = {};
             Backbone.history.start();
         });
 
-        alert('device ready');
+        elLatitude = document.querySelector('.latitude');
+        elLongitude = document.querySelector('.longitude');
 
-        // can we start listening to some 'location updated' event here?
+        elLatitude.innerHTML = 'loaded';
+
+        if (elLatitude && elLongitude){
+            elLongitude.innerHTML = 'locked in';
+
+
+            var options = { frequency: 3000, enableHighAccuracy: true };
+            watchId = navigator.geolocation.watchPosition(onSuccess, onError, options);
+
+            var currentOptions = { enableHighAccuracy: true, timeout: 10000 };
+            navigator.geolocation.getCurrentPosition(onSuccess, onError, currentOptions);
+
+            elLongitude.innerHTML = 'and events set';
+        }
     };
 
-    document.addEventListener('deviceready', this.onDeviceReady, false);
+    var onSuccess = function(position){
+      // obviously we'll get marionette to do this, but for now...
+        elLatitude.innerHTML = 'blah : ' + position.coords.latitude;
+        elLongitude.innerHTML = 'gah: ' + position.coords.longitude;
+    };
+
+    var onError = function() {
+        // some logging would be nice too...
+        //navigator.geolocation.clearWatch(watchId);
+        //alert('bah!');
+        elLatitude.innerHTML = 'bah!!';
+        console.writeln('gps lookup failed');
+    };
+
+    document.addEventListener('deviceready', onDeviceReady, false);
 
 }(GeoTest));
 
